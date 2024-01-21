4145969e24fc94a6388e15ad14f6ab9ca73058a3
Sai Kumar Kanuri <saikumar.kanuri.ksvs@gmail.com>
2024-01-21 21:27:23 +0530

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,163 +1,126 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../firebase';
 import { useNavigate } from 'react-router-dom';
-
-
-
-// const Navbar = () => {
-
-//     const [isLoggedIn, setIsLoggedIn] = useState(false);
-//     const navigate = useNavigate();
-//     // const accessToken = localStorage.getItem('accessToken');
-//     // useEffect(() => {
-//     //     // Check if there's an access token in local storage
-
-
-//     //     // Update the isLoggedIn state based on the presence of the access token
-//     //     setIsLoggedIn(!!accessToken);
-//     // }, [accessToken]);
-
-// const handleLogout = async () => {
-//     try {
-//         // Add Firebase sign-out logic here
-//         await auth.signOut();
-
-//         localStorage.removeItem('accessToken');
-
-
-//         // // Update the local state to reflect the user's authentication status
-//         setIsLoggedIn(false);
-
-//         // Redirect or perform additional actions after logout
-//         navigate('/signin');
-//     } catch (error) {
-//         console.error('Error logging out:', error);
-//     }
-// }
-
-//     // useEffect(() => {
-
-//     // })
-
-//     const [user, setUser] = useState(null);
-
-//     const handleSignOut = async () => {
-//         try {
-//             await auth.signOut();
-//             setIsLoggedIn(false);
-//             navigate('/');
-//         } catch (error) {
-//             console.error(error.message);
-//         }
-//     };
-
-//     useEffect(() => {
-//         const unsubscribe = auth.onAuthStateChanged((user) => {
-//             setUser(user)
-//             setIsLoggedIn(!!user); // Set isLoggedIn to true if the user is logged in, false otherwise
-//         });
-
-//         return () => unsubscribe();
-//     }, []);
-
-//     return (
-//         <nav className='bg-blue-600 p-4 text-white text-lg'>
-//             <div className='flex justify-between'>
-//                 <div className='flex items-center'>
-//                     <img
-//                         src='https://preview.redd.it/hi-this-is-a-logo-for-the-task-manager-application-called-v0-si3hzlaglc7b1.png?width=640&crop=smart&auto=webp&s=04d231d246026a59f988ac183a82e0ea2ca8ef4e'
-//                         alt='Logo'
-//                         className='mr-2 w-10 h-10 rounded-full'
-//                     />
-
-//                     <Link to="/">
-//                         Home
-//                     </Link>
-//                 </div>
-//                 {!isLoggedIn ? (
-//                     <div className='flex justify-evenly'>
-//                         <div>
-//                             <Link className="m-2 p-2 rounded-md bg-blue-800 hover:bg-blue-400" to="/signin">
-//                                 Sign In
-//                             </Link>
-//                         </div>
-//                         <div>
-//                             <Link className="m-2 bg-blue-400 text-white p-2 rounded-md hover:bg-blue-800 hover:text-white" to="/signUp">
-//                                 Sign Up
-//                             </Link>
-//                         </div>
-//                     </div>
-//                 ) : (
-//                     <div>
-//                         <button
-//                             onClick={handleSignOut}
-//                             className="m-1 p-2 rounded-md bg-blue-800 hover:bg-blue-400 text-white"
-//                         >
-//                             Log Out
-//                         </button>
-//                     </div>
-//                 )}
-
-//             </div>
-//         </nav>
-//     )
-// }
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 
 const Navbar = ({ user }) => {
-    // console.log(user);
     const navigate = useNavigate();
+    const [menuOpen, setMenuOpen] = useState(false);
+
     const handleSignout = async () => {
         try {
-            // Add Firebase sign-out logic here
             await auth.signOut();
-            navigate("/signin")
+            navigate("/signin");
             localStorage.removeItem('accessToken');
-
         } catch (error) {
             console.error('Error logging out:', error);
         }
-    }
+    };
+
     return (
         <nav className='bg-blue-600 p-4 text-white font-semibold text-lg'>
-            <div className='flex justify-between'>
-                <div className='flex items-center'>
+            <div className='container mx-auto flex justify-between items-center'>
+                <div>
                     <Link to='/' className='font-semibold font-serif'>
                         MyTask
                     </Link>
                 </div>
-                {user ? (
-                    <div className='flex justify-evenly'>
-                        <div className="m-1 p-2 rounded-md bg-blue-800 hover:bg-blue-400 text-white">
-                            <Link  to="/tasks">
-                                My Tasks
-                            </Link>
-                        </div>
-                        <div className="m-1 p-2 rounded-md bg-blue-800 hover:bg-blue-400 text-white">
-                            <Link  to="/tasks/create">
-                                Create Tasks
-                            </Link>
-                        </div>
-                        <div>
-                            <button className="m-1 p-2 rounded-md bg-blue-800 hover:bg-blue-400 text-white" onClick={handleSignout}>
-                                Log Out
-                            </button>
-                        </div>
-                    </div>
-                ) : (
-                    <div className='flex justify-evenly'>
-                        <div>
-                            <Link className="m-2 p-2 rounded-md bg-blue-800 hover:bg-blue-400" to="/signin">
-                                Sign In
-                            </Link>
-                        </div>
-                        <div>
-                            <Link className="m-2 bg-blue-400 text-white p-2 rounded-md hover:bg-blue-800 hover:text-white" to="/signup">
-                                Sign Up
-                            </Link>
+                {/* Responsive menu button for smaller devices */}
+                <div className="md:hidden">
+                    <button
+                        onClick={() => setMenuOpen(!menuOpen)}
+                        className="text-white focus:outline-none "
+                    >
+                        {/* Add your responsive menu icon, for example, a burger icon */}
+                        {/* &#9776; */}
+                        <MenuOutlinedIcon/>
+                    </button>
+                </div>
+                {/* Responsive menu for smaller devices */}
+                {menuOpen && (
+                    <div className="md:hidden absolute top-16 right-4 bg-blue-600 p-4 rounded">
+                        <div className="flex flex-col items-end">
+                            {user ? (
+                                <>
+                                    <Link
+                                        to="/tasks"
+                                        className="text-white hover:bg-blue-400 p-2 rounded my-1"
+                                    >
+                                        My Tasks
+                                    </Link>
+                                    <Link
+                                        to="/tasks/create"
+                                        className="text-white hover:bg-blue-400 p-2 rounded my-1"
+                                    >
+                                        Create Tasks
+                                    </Link>
+                                    <button
+                                        className="text-white hover:bg-blue-400 p-2 rounded my-1"
+                                        onClick={handleSignout}
+                                    >
+                                        Log Out
+                                    </button>
+                                </>
+                            ) : (
+                                <>
+                                    <Link
+                                        to="/signin"
+                                        className="text-white hover:bg-blue-400 p-2 rounded my-1"
+                                    >
+                                        Sign In
+                                    </Link>
+                                    <Link
+                                        to="/signup"
+                                        className="text-white hover:bg-blue-400 p-2 rounded my-1"
+                                    >
+                                        Sign Up
+                                    </Link>
+                                </>
+                            )}
                         </div>
                     </div>
                 )}
+                {/* Regular menu for larger devices */}
+                <div className='hidden md:flex justify-evenly'>
+                    {user ? (
+                        <>
+                            <div className="m-1 p-2 rounded-md bg-blue-800 hover:bg-blue-400 text-white">
+                                <Link to="/tasks">My Tasks</Link>
+                            </div>
+                            <div className="m-1 p-2 rounded-md bg-blue-800 hover:bg-blue-400 text-white">
+                                <Link to="/tasks/create">Create Tasks</Link>
+                            </div>
+                            <div>
+                                <button
+                                    className="m-1 p-2 rounded-md bg-blue-800 hover:bg-blue-400 text-white"
+                                    onClick={handleSignout}
+                                >
+                                    Log Out
+                                </button>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div>
+                                <Link
+                                    className="m-2 p-2 rounded-md bg-blue-800 hover:bg-blue-400"
+                                    to="/signin"
+                                >
+                                    Sign In
+                                </Link>
+                            </div>
+                            <div>
+                                <Link
+                                    className="m-2 bg-blue-400 text-white p-2 rounded-md hover:bg-blue-800 hover:text-white"
+                                    to="/signup"
+                                >
+                                    Sign Up
+                                </Link>
+                            </div>
+                        </>
+                    )}
+                </div>
             </div>
         </nav>
     );
